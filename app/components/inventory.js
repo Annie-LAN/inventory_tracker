60676d313ed03800cd355b4068e9376433c80cea
Aining Li <annieli2025@u.northwestern.edu>
2024-08-09 17:40:42 +1200

    improve ui

--- app/components/inventory.js
+++ app/components/inventory.js
@@ -91,103 +91,100 @@ export default function Inventory({ setInventory }) {
   return (
     <Box>
       {/* Inventory Items */}
-      <Box width="500px" border="1px solid #333" boxShadow={12} sx={{ pb: 2 }}>
-        <Box
-          height="60px"
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <Typography variant="h5" color="#333">
-            Inventory Items
-          </Typography>
-        </Box>
-        <Stack spacing={2} maxHeight="500px" overflow="auto">
-          {inventory.map(({ name, quantity }, index) => (
-            <Box key={name}>
-              <Box
-                display="flex"
-                alignItems="center"
-                justifyContent="space-between"
-                sx={{ px: 4 }}
-              >
-                <Typography variant="body1" color="#333" textAlign="center">
-                  {name.charAt(0).toUpperCase() + name.slice(1)}
-                </Typography>
-                <Typography variant="body1" color="#333" textAlign="center">
-                  {quantity}
-                </Typography>
-                <Stack direction="row" spacing={2}>
-                  <Button
-                    variant="outlined"
-                    size="small"
-                    onClick={() => addItem(name, 1)}
-                  >
-                    Increment
-                  </Button>
-                  <Button
-                    variant="outlined"
-                    size="small"
-                    onClick={() => decrementItem(name)}
-                  >
-                    Decrement
-                  </Button>
-                  <Button
-                    variant="outlined"
-                    size="small"
-                    onClick={() => removeItem(name)}
-                  >
-                    Remove
-                  </Button>
-                </Stack>
+      <Card sx={{ width: "600px", my: 2 }}>
+        <CardContent>
+          <Box
+            height="60px"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Typography variant="h5" color="#333">
+              Inventory Items
+            </Typography>
+          </Box>
+          <Stack spacing={2} maxHeight="500px" overflow="auto">
+            {inventory.map(({ name, quantity }, index) => (
+              <Box key={name}>
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="space-between"
+                  sx={{ px: 4 }}
+                >
+                  <Typography variant="body1" color="#333" textAlign="center">
+                    {name.charAt(0).toUpperCase() + name.slice(1)}
+                  </Typography>
+                  <Typography variant="body1" color="#333" textAlign="center">
+                    {quantity}
+                  </Typography>
+                  <Stack direction="row" spacing={2}>
+                    <Button
+                      variant="outlined"
+                      size="small"
+                      onClick={() => addItem(name, 1)}
+                    >
+                      Increment
+                    </Button>
+                    <Button
+                      variant="outlined"
+                      size="small"
+                      onClick={() => decrementItem(name)}
+                    >
+                      Decrement
+                    </Button>
+                    <Button
+                      variant="outlined"
+                      size="small"
+                      onClick={() => removeItem(name)}
+                    >
+                      Remove
+                    </Button>
+                  </Stack>
+                </Box>
+                {index < inventory.length - 1 && <Divider variant="middle" />}
               </Box>
-              {index < inventory.length - 1 && <Divider variant="middle" />}
-            </Box>
-          ))}
-        </Stack>
-      </Box>
+            ))}
+          </Stack>
+        </CardContent>
+      </Card>
 
-      {/* Add item  */}
-      <Box
-        width="500px"
-        border="1px solid #333"
-        boxShadow={12}
-        p={4}
-        display="flex"
-        flexDirection="column"
-      >
-        <Typography variant="h5" color="#333">
-          Add Item
-        </Typography>
-        <TextField
-          required
-          id="outlined-required"
-          label="Name"
-          defaultValue=""
-          margin="dense"
-          value={itemName}
-          onChange={(e) => setItemName(e.target.value)}
-        />
-        <TextField
-          required
-          id="outlined-required"
-          label="Quantity"
-          defaultValue=""
-          margin="dense"
-          value={addQuantity}
-          onChange={(e) => setAddQuantity(e.target.value)}
-        />
-        <Button
-          variant="contained"
-          onClick={() => {
-            addItem(itemName, addQuantity);
-            setItemName("");
-            setAddQuantity(""); // want to reset this to None
-          }}
-        >
-          Add
-        </Button>
-      </Box>
+      {/* Add item */}
+      <Card sx={{ width: "500px" }}>
+        <CardContent>
+          <Typography variant="h5" color="#333">
+            Add Item
+          </Typography>
+          <Box display="flex" flexDirection="column">
+            <TextField
+              required
+              id="outlined-required"
+              label="Name"
+              margin="dense"
+              value={itemName}
+              onChange={(e) => setItemName(e.target.value)}
+            />
+            <TextField
+              required
+              id="outlined-required"
+              label="Quantity"
+              margin="dense"
+              value={addQuantity}
+              onChange={(e) => setAddQuantity(e.target.value)}
+            />
+            <Button
+              variant="contained"
+              onClick={() => {
+                addItem(itemName, addQuantity);
+                setItemName("");
+                setAddQuantity("");
+              }}
+            >
+              Add
+            </Button>
+          </Box>
+        </CardContent>
+      </Card>
 
       {/* Snackbar for errors */}
       <Snackbar
@@ -196,7 +193,7 @@ export default function Inventory({ setInventory }) {
         onClose={handleSnackbarClose}
       >
         <Alert onClose={handleSnackbarClose} severity="error">
-          {"Invaid Input"}
+          {"Invalid Input"}
         </Alert>
       </Snackbar>
     </Box>
